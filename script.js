b8d1ea102e11ff78d9d06bdb88a796d6346a89e8
sunday stanley Idung <sundaystanley56@gmail.com>
2021-07-07 01:17:43 +0100

    added more the progress setting function to my player

--- script.js
+++ script.js
@@ -55,6 +55,20 @@ function nextSong(){
       loadSong(songs[songIndex])
       playSong()
 }
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime/duration)*100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width)* duration
+}
 // Event litener
 playBtn.addEventListener('click', () => { 
     const isPlaying = musicContainer.classList.contains('play');
@@ -70,3 +84,6 @@ playBtn.addEventListener('click', () => {
 
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+audio.addEventListener('timeupdate', updateProgress)
+progressContainer.addEventListener('click', setProgress)
+audio.addEventListener('ended', nextSong)
